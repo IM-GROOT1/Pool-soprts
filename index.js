ceb66104f2ca019ae55927d368b206b944566d38
ayan biswas <ayanb4784@gmail.com>
2024-01-07 00:56:56 +0530

    change 4

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const togglebtn = document.querySelector(".toggle")
-const menulist = document.querySelector("nav")
 
-togglebtn.addEventListener("click",function(){
-    menulist.classList.toggle("active")
-})
+const bd = document.querySelector('nav')
+const btnOpen = document.querySelector('#open')
+const btnClose = document.querySelector('#close')
+
+
+function addNav(){
+bd.classList.add('active')
+}
+
+function removeNav(){
+bd.classList.remove('active')
+}
+
+btnOpen.addEventListener('click', addNav)
+btnClose.addEventListener('click', removeNav)
